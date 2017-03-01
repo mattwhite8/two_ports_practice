5df28ed4091d3e04480556715d22418152ff9fd6
Matt White <mattwhite1337@gmail.com>
2017-02-28 19:23:29 -0500

    finish exercise

--- twoPorts.js
+++ twoPorts.js
@@ -2,8 +2,15 @@ var http = require('http');
 var PORT1 = 7000;
 var PORT2 = 7500;
 
+var array = ["You rock!", "You're the best!", "Never give up!", "You make this look easy!"];
+
+function random(){
+	var randomNumber = Math.floor(Math.random() * array.length);
+	return array[randomNumber];
+}
+
 function handleNiceRequest(request, response){
-	response.end("You rock!");
+	response.end(random());
 };
 
 var niceServer = http.createServer(handleNiceRequest);
--- twoPorts1.js
+++ twoPorts1.js
@@ -2,8 +2,15 @@ var http = require('http');
 var PORT1 = 7000;
 var PORT2 = 7500;
 
+var array = ["You do not rock!", "You're definitely not the best!", "Give up!", "You make this look hard!"];
+
+function random(){
+	var randomNumber = Math.floor(Math.random() * array.length);
+	return array[randomNumber];
+}
+
 function handleMeanRequeset(request, response){
-	response.end("You do not rock");
+	response.end(random());
 };
 
 var meanServer = http.createServer(handleMeanRequeset);
